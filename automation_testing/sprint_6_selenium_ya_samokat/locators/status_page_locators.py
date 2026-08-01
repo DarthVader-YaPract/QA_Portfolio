729c5a25e7619fe97359f79a83592026745bbfb7
Andrey Kalashov <incomin-post@yandex.ru>
2026-08-01 15:46:50 +0000

    Удаление старой папки sprint_6_selenium_ya_samokat

--- automation_testing/sprint_6_selenium_ya_samokat/locators/status_page_locators.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class StatusPageLocators:
-    NOT_FOUND_IMAGE = (By.XPATH, "//img[contains(@src, 'not-found')] | //div[contains(@class, 'Track_NotFound')]")
-
--- automation_testing/sprint_6_selenium_ya_samokat/pages/status_page.py
+++
@@ -1,8 +0,0 @@
-from locators.status_page_locators import StatusPageLocators
-from pages.base_page import BasePage
-
-
-class StatusPage(BasePage):
-    def is_not_found_visible(self):
-        return self.is_visible(StatusPageLocators.NOT_FOUND_IMAGE)
-
